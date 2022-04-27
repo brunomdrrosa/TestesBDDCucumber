e60075bfa7390b9374bde20576701ef2f58fa7bc
Bruno Machado <brunomdr46@gmail.com>
2022-04-27 14:52:56 -0300

    Scenario Outline

--- src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -12,6 +12,7 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 
 public class PropondoLanceSteps {
@@ -32,39 +33,49 @@ public class PropondoLanceSteps {
 		System.out.println("After");
 	}
 	
-	@Dado("um lance valido")
+	@Dado("um lance válido")
 	public void dado_um_lance_valido() {
 		Usuario usuario = new Usuario("fulano");
 		lance = new Lance(usuario , BigDecimal.TEN);
 	}
 
-	@Quando("propoe ao leilao")
+	@Quando("propõe ao leilão")
 	public void quando_propoe_o_lance() {
 		leilao.propoe(lance);
 	}
 	
-	@Entao("o lance eh aceito")
-	public void entao_o_lance_eh_aceito() {
+	@Então("o lance é aceito")
+	public void entao_o_lance_e_aceito() {
 		Assert.assertEquals(1, leilao.getLances().size());
 		Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
 	}
 	
-	@Dado("um lance de {double} reais do usuario {string}")
+	@Dado("um lance de {double} reais do usuário {string}")
 	public void um_lance_de_reais_do_usuario_fulano(Double valor, String nomeUsuario) {
 		Lance lance = new Lance(new Usuario(nomeUsuario), new BigDecimal(valor));
 		lista.add(lance);
 	}
 
-	@Quando("propoe varios lances ao leilao")
+	@Quando("propõe vários lances ao leilão")
 	public void propoe_varios_lances_ao_leilao() {
 		this.lista.forEach(lance -> leilao.propoe(lance));
 	}
 	
-	@Entao("os lances sao aceitos")
+	@Então("os lances são aceitos")
 	public void os_lances_sao_aceitos() {
 		Assert.assertEquals(this.lista.size(), leilao.getLances().size());
 		Assert.assertEquals(this.lista.get(0).getValor(), leilao.getLances().get(0).getValor());
 		Assert.assertEquals(this.lista.get(1).getValor(), leilao.getLances().get(1).getValor());
+	}
+	
+	@Dado("um lance inválido de {double} reais e do usuário {string}")
+	public void um_lance_inválido_de_reais(Double valor, String nomeUsuario) {
+		this.lance = new Lance(new BigDecimal(valor));
+	}
+
+	@Então("o lance não é aceito")
+	public void o_lance_nao_e_aceito() {
+		Assert.assertEquals(0, leilao.getLances().size());
 	}
 	
 }
--- src/test/resources/features/propondo_lances.feature
+++ src/test/resources/features/propondo_lances.feature
@@ -1,14 +1,24 @@
 # language: pt
 
-Funcionalidade: Propondo lances ao leilao
+Funcionalidade: Propondo lances ao leilão
 
-Cenario: Propondo um unico lance valido
- Dado um lance valido
- Quando propoe ao leilao
- Entao o lance eh aceito
+Cenário: Propondo um único lance válido
+ Dado um lance válido
+ Quando propõe ao leilão
+ Então o lance é aceito
  
-Cenario: Propondo varios lances validos
- Dado um lance de 10.0 reais do usuario "fulano"
- E um lance de 15.0 reais do usuario "beltrano"
- Quando propoe varios lances ao leilao
- Entao os lances sao aceitos
+Cenário: Propondo vários lances válidos
+ Dado um lance de 10.0 reais do usuário "fulano"
+ E um lance de 15.0 reais do usuário "beltrano"
+ Quando propõe vários lances ao leilão
+ Então os lances são aceitos
+ 
+Esquema do Cenário: Propondo um lance inválido
+ Dado um lance inválido de <valor> reais e do usuário '<nomeUsuario>'
+ Quando propõe ao leilão
+ Então o lance não é aceito
+ 
+ Exemplos:
+  | valor | nomeUsuario |
+  |    0  |    fulano   |
+  |   -1  |   beltrano  |
